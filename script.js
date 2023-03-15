149e2a4ce943bef6d4e916fa3e23b8ad54d5123e
Devin Clarke <devinlclarke@gmail.com>
2023-03-15 04:13:44 -0400

    React Component Practice

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
-var Temperature = function Temperature(props) {
-  console.log('props: ', props);
+
+var App = function App() {
   return React.createElement(
-    'h1',
+    Template,
     null,
-    'The current temperature is ',
-    props.degree,
-    ' degree ',
-    props.unit
+    React.createElement(
+      'h1',
+      null,
+      'Main content'
+    )
   );
 };
 
-var element = React.createElement(Temperature, { degree: 25, unit: 'celsius' });
-
-ReactDOM.render(element, document.getElementById('root'));
+ReactDOM.render(React.createElement(App, null), document.getElementById('root'));
--- src/script.js
+++ src/script.js
@@ -1,11 +1,13 @@
-const Temperature = (props) => {
-  console.log('props: ', props);
-  return <h1>The current temperature is {props.degree} degree {props.unit}</h1>;
-};
 
-const element = <Temperature degree={25} unit="celsius" />;
+const App = () => {
+  return (
+    <Template>
+      <h1>Main content</h1>
+    </Template>
+  );
+}
 
 ReactDOM.render(
-  element,
+  <App />,
   document.getElementById('root')
 );
